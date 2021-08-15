19b4f56519a529e139ead339292715a892e3a9f5
Dante Carrasco <arielcarp1996@gmail.com>
2021-08-15 12:16:58 -0300

    Trabajo terminado

--- js/script.js
+++ js/script.js
@@ -1,41 +1,47 @@
 addEventListener("DOMContentLoaded", () => {
     const menuButton = document.getElementById("main-menu-toggle")
-    const navMenu = document.getElementById("nav")
-    const menu = document.getElementById("menu")
+    const navMenu = document.getElementById("nav") // VARIABLE en la que se ALMACENA el NAVEGADOR PRINCIPAL.
+    const menu = document.getElementById("menu") // VARIABLE en la que se ALMACENA el MENU dentro del NAVEGADOR.
+    const buttons = document.querySelectorAll("button,.button") // VARIABLE en la que se ALMACENAN TODOS los BOTONES.
+
+    // Cada vez que se haga CLICK sobre UNO de los ITEMS dentro del MENU, ESTE se OCULTARA.
     const menuItems = menu.querySelectorAll("li")
-    for(let item of menuItems){
-        item.addEventListener("click", () => {
-            if (navMenu.classList.contains("show")){
-                navMenu.classList.toggle("show")
-            }
-        })
+    if (menuItems){
+        for(let item of menuItems){
+            item.addEventListener("click", () => {
+                if (navMenu.classList.contains("show")){
+                    navMenu.classList.toggle("show")
+                }
+            })
+        }
     }
 
-    /* En DISPOSITIVOS CHICOS, le quito al ELEMENTO perteneciente a la VARIABLE "menu", la CLASE "menu-hover", y le AGREGO 
-    la CLASE "menu-active. */
     const smallBp = matchMedia("(max-width: 500px)")
-    const quitHover = () => {
-        if (smallBp.matches){
-            for (let item of menuItems){
-                if (item.classList.contains("menu-hover"))
+    const quitHover = (breakpoint,component,list) => {
+        if (breakpoint.matches){
+            for (let item of list){
+                if (item.classList.contains(`${component}-hover`))
                 {
-                    item.classList.toggle("menu-hover")
-                    item.classList.toggle("menu-active")
+                    item.classList.toggle(`${component}-hover`)
+                    item.classList.toggle(`${component}-active`)
                 }
             }
         }
         else
         {
-            for (let item of menuItems){
-                if (item.classList.contains("menu-hover") == false)
+            for (let item of list){
+                if (item.classList.contains(`${component}-hover`) == false)
                 {
-                    item.classList.toggle("menu-hover")
-                    item.classList.toggle("menu-active")
+                    item.classList.toggle(`${component}-hover`)
+                    item.classList.toggle(`${component}-active`)
                 }
             }
         }
     }
-    quitHover()
+    /* En DISPOSITIVOS CHICOS, le quito a los ELEMENTOS pertenecientes a la VARIABLE "menu" y "buttons", la PSEUDOCLASE "hover" y
+    le agrego la PSEUDOCLASE "active". */
+    quitHover(smallBp,"menu",menuItems)
+    quitHover(smallBp, "button", buttons)
 
     /* Si en el "breakpoint" indicado en la VARIABLE "mediaBp", el ELEMENTO perteneciente a la VARIABLE "navMenu" 
     contiene la CLASE "show", la ELIMINO. */
@@ -52,11 +58,16 @@ addEventListener("DOMContentLoaded", () => {
 
     // La FUNCIONES "hideMenu" y "quitHover" van a EJECUTARSE cada vez que se REDIMENSIONE la PANTALLA.
     window.addEventListener("resize", () => {
-        hideMenu()
-        quitHover()
+        if (mediaBp){
+            hideMenu()
+        }
+        quitHover(smallBp,"menu",menuItems)
+        quitHover(smallBp, "button", buttons)
     })
     
-    menuButton.addEventListener("click", () => {
-        navMenu.classList.toggle("show")
-    })
+    if (menuButton){
+        menuButton.addEventListener("click", () => {
+            navMenu.classList.toggle("show")
+        })
+    }
 })
